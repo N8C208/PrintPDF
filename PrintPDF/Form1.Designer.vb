aef1d21fe397ab4b7475e94244dbcf91a28e5099
Nathan Carter <Nathan.Carter@ad.bmefire.com>
2022-02-09 11:02:40 -0700

    Add project files.

---
+++ PrintPDF/Form1.Designer.vb
@@ -0,0 +1,106 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(3, 19)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(3, 25, 3, 3)
+        Me.TextBox1.MaximumSize = New System.Drawing.Size(80, 0)
+        Me.TextBox1.MinimumSize = New System.Drawing.Size(80, 40)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(80, 40)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.AllowDrop = True
+        Me.Button1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.Button1.Location = New System.Drawing.Point(3, 85)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Print All Files"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.Location = New System.Drawing.Point(3, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Add Files to Print"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(101, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(0, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(101, 46)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(120, 111)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.MinimumSize = New System.Drawing.Size(135, 150)
+        Me.Name = "Form1"
+        Me.Padding = New System.Windows.Forms.Padding(3)
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ PrintPDF/Form1.vb
@@ -0,0 +1,86 @@
+﻿Option Explicit On
+
+Public Class Form1
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Public Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim filewithext As String
+        Dim i As Long
+        Dim swtime As Integer
+        Dim sw As New Stopwatch
+
+        'Get files from user input in Textbox 1
+        Dim files1 As String = (TextBox1.Text)
+
+        'Set file location and file extension
+        Dim workingDir As String = ("C:\SWPDM\PDF's\")
+        Dim fileExt As String = (".pdf")
+
+        'Split user input into array, seperated by line break
+        Dim stringText = files1.Split(New String() {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries)
+        Dim stringArray(stringText.Length - 1) As String
+
+        'Set form size to autofit height based upon number of lines input
+        Dim x As Integer = Me.ClientSize.Width
+        Dim y As Integer = Me.ClientSize.Height
+        y = ((stringText.Length + 1) * 13)
+        Me.TextBox1.Size = New Size(x, y - 10)
+        Me.ClientSize = New Size(x + 50, y + 40)
+
+        'Start new process to print PDF's
+        Dim psi As New ProcessStartInfo
+        psi.UseShellExecute = True
+        psi.Verb = "print"
+        psi.WindowStyle = ProcessWindowStyle.Hidden        'IF NEEDED, ADD TO NEXT LINE: (psi.Arguments = PrintDialog1.PrinterSettings.PrinterName.ToString()) 
+
+        'Assign full path to each file and execute print process
+        For i = 0 To UBound(stringArray)
+            filewithext = (workingDir + stringText(i) & fileExt)
+            stringArray(i) = filewithext
+
+            'OPTIONAL METHOD OF PRINTING, USES PDFTOPRINTER APPLICATION
+            'cmdHandle(filewithext)
+
+
+            psi.FileName = (filewithext) 'document to be printed
+            Process.Start(psi)
+
+            'Set text to show user current print info
+            Label2.Text = ("Now Printing")
+            Label3.Text = psi.FileName
+
+            'set time delay for print info display
+            swtime = 3000
+            sw.Start()
+            Do While sw.ElapsedMilliseconds < swtime
+                Application.DoEvents()
+            Loop
+            sw.Stop()
+        Next
+
+        'Clear User input text box and show job is complete
+        TextBox1.Text = ""
+        Label2.Text = "Print Job Complete"
+        Label3.Text = ""
+    End Sub
+
+    'Public Sub cmdHandle(ByVal pstringArray As String)
+    'Dim myProcess As New Process
+    'Dim quote As String = ("""")
+
+    'myProcess.StartInfo.UseShellExecute = True
+    'myProcess.StartInfo.WorkingDirectory = ("C:\SWPDM\PDF'S\")
+    'myProcess.StartInfo.FileName = ("cmd /b")
+    'myProcess.StartInfo.Arguments = ("/c  \\bmedc-01\common\PrintPDFPackageTemp\PrintApp\PDFtoPrinter " + quote + pstringArray + quote)
+    'Debug.Print(myProcess.StartInfo.Arguments)
+
+    'myProcess = Process.Start(myProcess.StartInfo)
+    'myProcess.WaitForExit()
+    'System.Threading.Thread.Sleep(1500)
+    'Exit Sub
+    'End Sub
+End Class
+
